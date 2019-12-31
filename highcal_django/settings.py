09304179c6b02f1793f1e0eae72b640fb35218b8
connorblackburn98 <connor.blackburn.uk@gmail.com>
2019-12-31 10:58:18 +0000

    email integration set up, login in page now has a link for the user to use if password is forgotten. Lorem ipsum taking off index and log in page. password_reset_form.html created to give users the ability to reset passwor if needed, process will be completed through an email to users inbox

--- highcal_django/settings.py
+++ highcal_django/settings.py
@@ -133,8 +133,6 @@ STATIC_URL = '/static/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage" 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -143,3 +141,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587 
