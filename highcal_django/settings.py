75021f4db1459b32abff25eaebbe9018dbed5789
connorblackburn98 <connor.blackburn.uk@gmail.com>
2019-12-31 08:40:36 +0000

    heroku added to allowed hosts within settings.py

--- highcal_django/settings.py
+++ highcal_django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('gitpod_host')]
+ALLOWED_HOSTS = [os.environ.get('gitpod_host'), 'highcal-django.herokuapp.com']
 
 
 # Application definition
