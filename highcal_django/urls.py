342103ab4475c8ba227f74a780fbf03880194a82
connorblackburn98 <connor.blackburn.uk@gmail.com>
2019-12-31 13:28:13 +0000

    products.html file created so users can visit the shop section within the navbar and be directed to a list of the products. Products are getting pulled from database correctly

--- highcal_django/urls.py
+++ highcal_django/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts.views import index
 from accounts import urls as accounts_urls
+from products import urls as urls_products
+from products.views import all_products
 from django.views.generic import RedirectView
 from django.views.static import serve
 from .settings import MEDIA_ROOT
@@ -25,6 +27,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
     url(r'^accounts/', include(accounts_urls)),
+    url(r'^products/', include(urls_products)),
     url(r'^$', RedirectView.as_view(url='posts/')),
     url(r'^posts/', include('posts.urls')),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
