fff2995864547775c9d89408717532fb8b450e54
connorblackburn98 <connor.blackburn.uk@gmail.com>
2020-01-01 18:52:18 +0000

    checkout forms, urls + views created

--- highcal_django/urls.py
+++ highcal_django/urls.py
@@ -21,6 +21,7 @@ from products import urls as urls_products
 from products.views import all_products
 from cart import urls as urls_cart
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 from django.views.generic import RedirectView
 from django.views.static import serve
 from .settings import MEDIA_ROOT
@@ -31,6 +32,7 @@ urlpatterns = [
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^$', RedirectView.as_view(url='posts/')),
     url(r'^posts/', include('posts.urls')),
